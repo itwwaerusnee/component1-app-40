947d4d7bdd4186db9cac19831c12229289bc1cca
waerusnee wani <wwaerusnee@ptnicec.ac.th>
2018-11-08 18:10:42 +0700

    component140

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { AlertController, NavController, ActionSheetController } from 'ionic-angular';
+/* import { Title } from '@angular/platform-browser'; */
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,54 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public alertCtrl: AlertController,
+    public actionSheet: ActionSheetController,
+    public navCtrl: NavController) {
 
   }
 
+  showAction(){
+    const acSheet = this.actionSheet.create({
+      title: 'Test Action',
+    
+      buttons: [
+        {
+          text: 'Destructive',
+          role: 'destructive',
+          handler: () => {
+            console.log('Destructive clicked');
+          }
+        },{
+          text: 'Archive',
+          handler: () => {
+            console.log('Archive clicked');
+          }
+        },{
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    acSheet.present();
+  }
+  showAlert1(){
+    const alert = this.alertCtrl.create({
+      title: 'Config Answer!!',
+      subTitle: 'Waerusnee Wani',
+      buttons: [
+      {
+        text: 'Ok'
+      },{
+        text: 'cancel',
+        handler: () => {
+          console.log("Cancel");
+        },
+      }  
+      ]
+    });
+    alert.present();
+  }
 }
